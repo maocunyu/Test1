b5ea6e22bc2aa6b7da3ef4a9e3da22d97cd247a9
maocunyu <840676494@qq.com>
2018-03-12 14:12:19 +0800

    测试

---
+++ src/main/java/com/bdqn/test/Demo.java
@@ -0,0 +1,10 @@
+package com.bdqn.test;
+
+/**
+ * Created by 84067 on 2018/3/12.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("qwe");
+    }
+}
